aa1dfde93ff89df6c4145214d5449aa77146d415
HruthVik <hruthviknaik03@gmail.com>
2024-01-24 09:33:40 +0530

    Added api

---
+++ script.js
@@ -0,0 +1,13 @@
+const apiKey="c05458ff0e19cc433ecd8c36131ea92c";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+async function checkWeather(){
+    const response=await fetch(apiUrl + `&appid=${apiKey}`);
+    var data=await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+ "°c";
+    document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML= data.wind.speed+"km/h";
+    
+}
+checkWeather();
